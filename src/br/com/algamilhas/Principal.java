14d6b7ca640d38db5cec050f5eaa4e2e2f6cd42c
Hattsuyia <thiagoam@live.com>
2023-06-20 07:41:17 -0300

    Alterando modificadores de acesso.

---
+++ src/br/com/algamilhas/Principal.java
@@ -0,0 +1,10 @@
+package br.com.algamilhas;
+
+import br.com.algamilhas.pontuacao.Participante;
+
+public class Principal {
+    public static void main(String[] args) {
+        Participante participante1 = new Participante("Dinko");
+
+    }
+}
--- src/br/com/algamilhas/pontuacao/Participante.java
+++ src/br/com/algamilhas/pontuacao/Participante.java
@@ -1,21 +1,30 @@
+package br.com.algamilhas.pontuacao;
+
 import java.util.Objects;
 
 public class Participante {
-    String nome;
-    int saldoDePontos;
+    private String nome;
+    private int saldoDePontos;
 
     public Participante(String nome) {
         this (nome, 0);
     }
 
-    public Participante(String nome, int saldoDePontos) {
+    private Participante(String nome, int saldoDePontos) {
         Objects.requireNonNull(nome,"Não pode ser nome nulo");
 
         if(saldoDePontos<0){
             throw new IllegalArgumentException("O Saldo inicial não pode ser menor que zero");
         }
-
+        
         this.nome = nome;
         this.saldoDePontos = saldoDePontos;
+    }
+    public void creditarPontos (int pontos){
+        if(pontos<0){
+            throw new IllegalArgumentException("Não pode creditar pontos negativos");
+        }
+
+        this.saldoDePontos += pontos;
     }
 }
